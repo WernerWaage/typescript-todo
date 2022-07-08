ee620ed40b7e3f19828d3e79398b2fda8ce4c792
werner <werner.waage@gmail.com>
2022-07-08 16:35:15 +0200

    First build

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@
  **/
 
 import confetti from 'canvas-confetti';
+import { v4 as uuidv4 } from 'uuid';
+console.log(uuidv4());
 
 confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
   resize: true,
   useWorker: true,
 })({ particleCount: 200, spread: 200 });
+
+console.log("Typescript loaded");
--- src/script.ts
+++
@@ -1,4 +0,0 @@
-import {v4 as uuidV4} from 'uuid';
-uuidV4();
-
-console.log("Loaded")
