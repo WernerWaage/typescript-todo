0adda46529478fef222b9ac2180026e31c2c49ba
werner <werner.waage@gmail.com>
2022-07-11 19:43:24 +0200

    lit, mobx and lit-mobx

---
+++ src/classes/todo.class.ts
@@ -0,0 +1,20 @@
+import { makeObservable, observable, action } from "mobx"
+
+class Todo {
+    id = Math.random()
+    title = ""
+    finished = false
+
+    constructor(title:any) {
+        makeObservable(this, {
+            title: observable,
+            finished: observable,
+            toggle: action
+        })
+        this.title = title
+    }
+
+    toggle() {
+        this.finished = !this.finished
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 export {};
-  
+
+// Imports
 import { v4 as uuidv4 } from 'uuid'
 import type { Task } from './types/task.type';
 import { Person } from './classes/person.class'
-const person = new Person('John')
-console.log(uuidv4())
+import { makeObservable, observable, action } from "mobx"
+import { LitElement, html } from 'lit-element';
+import './src/components/my-elements.js';
 
+
+const person = new Person('John')
+
+console.log(uuidv4())
 console.log('Typescript loaded  🦾 👾')
 
 const list = document.querySelector<HTMLUListElement>('#list')
@@ -16,7 +22,10 @@ const input = document.querySelector<HTMLInputElement>('#new-task-title')
 // On load:
 const tasks: Task[] = loadTasks()
 tasks.forEach(addListItem)
+const loadtimes = getLoadTimes()
+console.log("Loaded : " + loadtimes + " times")
 
+// Events
 form?.addEventListener('submit', (e) => {
     e.preventDefault()
     if (input?.value == '' || input?.value == null) return
@@ -60,3 +69,20 @@ function loadTasks(): Task[] {
         return []
     }
 }
+
+
+// counter
+function getLoadTimes():number {
+  const loadtimesString = localStorage.getItem('LOADTIMES')
+  let loadtimes = 0;
+  if (loadtimesString) {
+    if (parseInt(loadtimesString) > 0) { loadtimes = parseInt(loadtimesString) } else { loadtimes = 0 }
+  }
+  loadtimes++;
+  localStorage.setItem('LOADTIMES', loadtimes.toString())
+  return loadtimes
+}
+
+
+
+
