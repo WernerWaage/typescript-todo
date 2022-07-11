9c702595e4b96bf2f23fbb8ab8ffa2c22d177d88
werner <werner.waage@gmail.com>
2022-07-11 23:34:08 +0200

    lit templates

--- src/components/my-element.ts
+++ src/components/my-element.ts
@@ -1,6 +1,7 @@
-import {LitElement, html} from 'lit-element';
+import {LitElement, html} from 'lit';
 
 class MyElement extends LitElement {
+
   render() {
     return html`
       <div>Hello from MyElement!</div>
