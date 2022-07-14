071d29f2de8d4d5e43257e8503a92106c97aafe3
werner <werner.waage@gmail.com>
2022-07-14 19:31:24 +0200

    Mobx and Lit is working (Observable property)

--- src/components/my-element.ts
+++ src/components/my-element.ts
@@ -1,5 +1,7 @@
 import { LitElement, html } from 'lit'
 
+// Just a super simple component that renders a greeting.
+// ps. remember to use MobxLitElement instead of LitElement if you are using reactive properties.
 class MyElement extends LitElement {
     render() {
         return html` <div>Hello from MyElement!</div> `
